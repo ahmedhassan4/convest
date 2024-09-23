cfdc56b8657cea9e066c0a7d6d2466cce35b3dce
ahmedhassan4 <158503741+ahmedhassan4@users.noreply.github.com>
2024-09-23 04:47:46 +0300

    Add e-commerce Routes and page Header for routers

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -10,13 +10,33 @@ export const routes = {
     home: "/",
     institutions: "/dashboard/institutions",
     all_courses: {
-    list: "/dashboard/all-courses",
-    course_detailes: (course_id: any | string) => `${routes.dashboard.all_courses.list}/${course_id}`, // Reusing the list property
-  },
+      list: "/dashboard/all-courses",
+      course_detailes: (course_id: any | string) =>
+        `${routes.dashboard.all_courses.list}/${course_id}`, // Reusing the list property
+    },
     users: "/dashboard/users",
     calendar: "/dashboard/calendar",
     myAccount: "/dashboard/myAccount",
     files: "/dashboard/files",
     semesters: "/dashboard/semesters",
+  },
+  eCommerce: {
+    dashboard: "/ecommerce",
+    products: "/ecommerce/products",
+    createProduct: "/ecommerce/products/create",
+    productDetails: (slug: string) => `/ecommerce/products/${slug}`,
+    ediProduct: (slug: string) => `/ecommerce/products/${slug}/edit`,
+    categories: "/ecommerce/categories",
+    createCategory: "/ecommerce/categories/create",
+    editCategory: (id: string) => `/ecommerce/categories/${id}/edit`,
+    orders: "/ecommerce/orders",
+    createOrder: "/ecommerce/orders/create",
+    orderDetails: (id: string) => `/ecommerce/orders/${id}`,
+    editOrder: (id: string) => `/ecommerce/orders/${id}/edit`,
+    reviews: "/ecommerce/reviews",
+    shop: "/ecommerce/shop",
+    cart: "/ecommerce/cart",
+    checkout: "/ecommerce/checkout",
+    trackingId: (id: string) => `/ecommerce/tracking/${id}`,
   },
 };
